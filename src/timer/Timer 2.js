44b0467451190e801ec64ae60ad01aa484bff4ab
Billy Galloway <galloway@olympus.F5Net.com>
2018-04-19 12:08:47 -0700

    eslint errors fixed

--- src/timer/Timer 2.js
+++ src/timer/Timer 2.js
@@ -1,19 +1,18 @@
-class Timer{
-  constructor(seconds){
+class Timer {
+  constructor(seconds) {
     this.seconds = seconds;
   }
 
-  start(){
-    let seconds = this.seconds;
-    let timerInterval = setInterval(function(){
-      if(seconds === 0){
+  start() {
+    let [seconds] = this.seconds;
+    const timerInterval = setInterval(() => {
+      if (seconds === 0) {
         clearInterval(timerInterval);
       }
       console.log(seconds);
       seconds -= 1;
     }, 1000);
   }
-
 }
 
 module.exports = Timer;
