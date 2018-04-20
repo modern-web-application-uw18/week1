264081493213b2d6609f8728f9bf0ae5a7cc6172
spencej4 <spencer.jack@alaskaair.com>
2018-04-20 13:46:16 -0700

    Adds assignments for Week 1

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,6 +1,13 @@
 const HTMLElement = require('./HTMLElement');
 
 // Define class here
-
+class DivElement extends HTMLElement {
+  constructor(content) {
+    super();
+    this.content = content;
+    this.tag = '<div>';
+    this.closingTag = '</div>';
+  }
+}
 // Export class here
-module.exports = {};
+module.exports = DivElement;
