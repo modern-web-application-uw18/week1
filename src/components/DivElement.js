af35693a92c608863724a60181286fb4781aa1b2
Jun Kim <chongjk88@hotmail.com>
2018-04-22 14:52:03 -0700

    Week 1 Assignment: ES6

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,6 +1,13 @@
 const HTMLElement = require('./HTMLElement');
 
 // Define class here
+class DivElement extends HTMLElement {
+    constructor(content) {
+        super(content);
+        this.tag = "div";
+        this.content = content;
+    }
+}
 
 // Export class here
 module.exports = {};
--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,17 +1,19 @@
-function Timer(seconds) {
-  this.seconds = seconds;
+//Defining class Timer
+class Timer {
+    constructor(seconds) {
+        this.seconds = seconds;
+    }
+    
+    start() {
+        const timerInterval = setInterval(function() {
+            if (this.seconds === 0) {
+                clearInterval(timerInterval);
+            };
+            console.log(this.seconds);
+            this.seconds -= 1;
+        }, 1000);
+    }
 }
 
-Timer.prototype.start = function() {
-  var instance = this;
-  var timerInterval = setInterval(function() {
-    if (instance.seconds === 0) {
-      clearInterval(timerInterval);
-    }
-
-    console.log(instance.seconds);
-    instance.seconds -= 1;
-  }, 1000);
-};
 
 module.exports = Timer;
