a89ae3e85cfb2ae507b0fcab8c203b61127e0944
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2018-04-23 14:21:52 -0700

    CFarris committing HTMLElement. js

--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,14 @@
-// Define class here
+class HTMLElement {
+  constructor(tag, content) {
+    this.tag = tag;
+    this.content = content;
+  }
+  render() {
+    return `<${this.tag}>${this.content}</${this.tag}>`;
+  }
+}
 
 // Export class here
-module.exports = {};
+module.exports = HTMLElement;
+const lovelaceQuote = new HTMLElement('p', 'I am never so happy as when I am really engaged in good earnest...');
+console.log(lovelaceQuote.render());
