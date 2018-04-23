ccd7d048cc881eb965fd2228a6c32bd99d7a29b9
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2018-04-23 14:21:21 -0700

    CFarris committing DivElement.js

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,6 +1,13 @@
 const HTMLElement = require('./HTMLElement');
 
 // Define class here
-
+class DivElement extends HTMLElement {
+  constructor(content) {
+    super('div', content);
+  }
+}
 // Export class here
-module.exports = {};
+
+module.exports = DivElement;
+const andIThinkToMyself = new DivElement('What a wonderful world');
+console.log(andIThinkToMyself.render());
