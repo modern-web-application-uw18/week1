0103ba6bc85de79d7aa56e644920e924f1323aee
tbreon <32778256+tbreon@users.noreply.github.com>
2018-04-23 16:41:59 -0500

    Homework 1
    Homework 1

--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,17 @@
 // Define class here
+class HTMLElement{
+    constructor(tag, content){
+        this.tag = tag;
+        this.content = content;       
+    }
+    render(){
+        return `<${this.tag}>${this.content}</${this.tag}>`;
+    }
+    
+}
 
+//const lovelaceQuote = new HTMLElement('p', 'I am never so happy as when I am really engaged in good earnest...');
+//console.log(lovelaceQuote.render());
 // Export class here
-module.exports = {};
+
+module.exports = HTMLElement;
