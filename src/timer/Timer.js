e8f827b2a0d4f65833effd1fe99eddb4f4e85322
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2018-04-25 15:29:28 -0700

    Cfarris comitting Timer.js

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,17 +1,24 @@
-function Timer(seconds) {
-  this.seconds = seconds;
+class Timer {
+  constructor(seconds) {
+    this.seconds = seconds;
+  }
+  start() {
+    const instance = this;
+    const timerInterval = setInterval(() => {
+      if (instance.seconds === 0) {
+        clearInterval(timerInterval);
+      }
+      console.log(instance.seconds);
+      instance.seconds -= 1;
+    }, 1000);
+  }
 }
 
-Timer.prototype.start = function() {
-  var instance = this;
-  var timerInterval = setInterval(function() {
-    if (instance.seconds === 0) {
-      clearInterval(timerInterval);
-    }
-
-    console.log(instance.seconds);
-    instance.seconds -= 1;
-  }, 1000);
-};
-
 module.exports = Timer;
+// export default Timer; // ES6 unsupported by node -v 8
+
+/* 1-3 Timer object is created
+   Timer prototype method is added that uses setInterval to countdown
+   for each instance, console.log the seconds,
+   export the time object so it can be imported by runTimer.
+*/
