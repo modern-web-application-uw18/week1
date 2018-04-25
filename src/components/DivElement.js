d692641fdeb5e916b30cbe92e88855ad7b61ee5f
Morgan Lang <morgan@dead-line.org>
2018-04-25 23:43:22 +0000

    fixed module export. it works now.

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -9,4 +9,4 @@ class DivElement extends HTMLElement {
 const Divstuff = new DivElement('This is content from DivElement.js. This could be used for composing content that needs to be updated from a db and then spat out to different pages. Handy!'); 
 console.log('test output from DivElement: ', Divstuff.render());
 // Export class here
-module.exports = {DivElement};
+module.exports = DivElement;
