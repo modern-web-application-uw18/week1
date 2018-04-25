79dfa5910dd9a8cc0b3eab9a0c7bdacd343abfec
Morgan Lang <morgan@dead-line.org>
2018-04-25 18:01:46 +0000

    DivElement v3

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -4,6 +4,7 @@ const Divstuff = new DivElement("This is content from DivElement.js")
 // Define class here
 class DivElement extends HTMLElement {
  constructor(content) {("div", content);
+ console.log(Divstuff.render());
  }
 }
 // Export class here
