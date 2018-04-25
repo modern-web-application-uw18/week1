a8c9cf5be1e3974b08cd4d9689341789dc23455f
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2018-04-25 15:28:45 -0700

    Cfarris committing HTMLElement.js

--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -8,7 +8,10 @@ class HTMLElement {
   }
 }
 
-// Export class here
-module.exports = HTMLElement;
+module.exports.default = HTMLElement;
+// export default HTMLElement; // ES6 unsupported by node
 const lovelaceQuote = new HTMLElement('p', 'I am never so happy as when I am really engaged in good earnest...');
 console.log(lovelaceQuote.render());
+
+const div = new HTMLElement('div', 'University of Washington');
+console.log(div.render());
