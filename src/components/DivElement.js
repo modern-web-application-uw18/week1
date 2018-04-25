8939a5318e28fef032b582a49600311b34765919
Morgan Lang <morgan@dead-line.org>
2018-04-25 23:07:14 +0000

    fixed console output

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,11 +1,14 @@
 const HTMLElement = require("./HTMLElement");
-const Divstuff = new DivElement("This is content from DivElement.js") 
+const Divstuff = new DivElement("This is content from DivElement.js. This could be used for composing content that needs to be updated from a db and then spat out to different pages. Handy!") 
 
 // Define class here
 class DivElement extends HTMLElement {
- constructor(content) {("div", content);
- console.log(Divstuff.render());
+ constructor(content) {
+   ("div", content);
+ 
+   console.log(Divstuff.render());
  }
 }
+console.log("test output from DivElement: ", Divstuff);
 // Export class here
 module.exports = {DivElement};
