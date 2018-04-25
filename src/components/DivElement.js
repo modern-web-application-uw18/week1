ba0140c05b4bf5cd426a76263d083306e8fa320c
Morgan Lang <morgan@dead-line.org>
2018-04-25 23:20:15 +0000

    cleanup, added super to get to top of divelement

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,12 +1,12 @@
-const HTMLElement = require("./HTMLElement");
-const Divstuff = new DivElement("This is content from DivElement.js. This could be used for composing content that needs to be updated from a db and then spat out to different pages. Handy!"); 
+const HTMLElement = require('../src/HTMLElement');
+const Divstuff = new DivElement('This is content from DivElement.js. This could be used for composing content that needs to be updated from a db and then spat out to different pages. Handy!'); 
 
 // Define class here
 class DivElement extends HTMLElement {
   constructor(content) {
-    ("div", content);
- }
+    super('div', content);
+  }
 }
-console.log("test output from DivElement: ", Divstuff.render());
+console.log('test output from DivElement: ', Divstuff.render());
 // Export class here
 module.exports = {DivElement};
