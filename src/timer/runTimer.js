d9aa9bfb80d76251766ca30978b664d25f3f38de
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2018-04-25 15:29:04 -0700

    CFarris committing runTimer.js

--- src/timer/runTimer.js
+++ src/timer/runTimer.js
@@ -1,4 +1,9 @@
 const Timer = require('./Timer');
+// import Timer from './Timer';
 
 const countdown = new Timer(10);
 countdown.start();
+
+// Line 1 imports the timer class from Timer.js
+// const countdown creates a new instance of the time object with 10 seconds called "countdown"
+// the "countdown" object calls the .start() method which begins the countdown.
