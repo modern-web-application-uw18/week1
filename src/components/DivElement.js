6c3451531115adaaead82cbdf0aa3e6e027c903d
Craig McMahan <tcraigmcmahan@comcast.net>
2018-04-26 16:33:33 -0700

    Week1 Hmwk - Craig McMahan

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -2,5 +2,16 @@ const HTMLElement = require('./HTMLElement');
 
 // Define class here
 
+class DivElement extends HTMLElement {
+  constructor(content) {
+    super();
+    this.content = content;
+  }
+
+  render() {
+    return `<div>${this.content}</div>`;
+  }
+}
+
 // Export class here
-module.exports = {};
+module.exports = DivElement;
