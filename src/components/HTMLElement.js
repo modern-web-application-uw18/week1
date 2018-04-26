de42eb6d4b0f2d08d8006e2f99ee1b7010794f29
Ryan Burke <32886083+rburke012@users.noreply.github.com>
2018-04-25 19:49:49 -0700

    Assignment 1 complete

--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,19 @@
+
 // Define class here
+class HTMLElement {
+    constructor(tag, content) {    
+      this.tag = tag;
+      this.content = content;
+    }
+    render() {
+        let open = '<' + this.tag + '>';
+      let close = '</' + this.tag + '>';
+      return open + this.content + close;
+    }
+  }
+  
+const newIdea = new HTMLElement("p","So nice to see you!")
+console.log(newIdea.render());
 
 // Export class here
-module.exports = {};
+module.exports = HTMLElement;
