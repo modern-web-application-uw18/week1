32db119ed9810b6ca03dbbb1a42ab582ca7c7648
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2018-04-26 02:46:58 -0700

    fixed spacing and exports

--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,3 +1,5 @@
+
+
 class HTMLElement {
   constructor(tag, content) {
     this.tag = tag;
@@ -8,10 +10,12 @@ class HTMLElement {
   }
 }
 
-module.exports.default = HTMLElement;
-// export default HTMLElement; // ES6 unsupported by node
+
 const lovelaceQuote = new HTMLElement('p', 'I am never so happy as when I am really engaged in good earnest...');
 console.log(lovelaceQuote.render());
 
 const div = new HTMLElement('div', 'University of Washington');
 console.log(div.render());
+
+module.exports = HTMLElement;
+// export default HTMLElement; // ES6 unsupported by node
