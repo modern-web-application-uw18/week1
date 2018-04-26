ca939bfb29a7a286ef6992f69ac5d10eee659e3b
Don Sando <sandodon1@gmail.com>
2018-04-26 16:21:21 -0700

    Homework 1 submission

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,6 +1,14 @@
 const HTMLElement = require('./HTMLElement');
 
 // Define class here
+class DivElement extends HTMLElement {
+  constructor(content) {
+    super(content);
+    this.content = content;
+  }
+  render() {
+    return `<div>${this.content}</div>`;
+  }
+}
 
-// Export class here
-module.exports = {};
+module.exports = DivElement;
