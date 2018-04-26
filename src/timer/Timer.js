36baee83b06a48dafc3bf5168f2f5ca10ff76bb2
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2018-04-26 02:47:27 -0700

    fixed spacing and export

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -3,22 +3,14 @@ class Timer {
     this.seconds = seconds;
   }
   start() {
-    const instance = this;
     const timerInterval = setInterval(() => {
-      if (instance.seconds === 0) {
+      if (this.seconds === 0) {
         clearInterval(timerInterval);
       }
-      console.log(instance.seconds);
-      instance.seconds -= 1;
+      console.log(this.seconds);
+      this.seconds -= 1;
     }, 1000);
   }
 }
 
 module.exports = Timer;
-// export default Timer; // ES6 unsupported by node -v 8
-
-/* 1-3 Timer object is created
-   Timer prototype method is added that uses setInterval to countdown
-   for each instance, console.log the seconds,
-   export the time object so it can be imported by runTimer.
-*/
