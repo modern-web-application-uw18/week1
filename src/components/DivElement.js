42a15a5abeaa365443c257664d3fff7639fadbea
misseswaddles <31780123+Misseswaddles@users.noreply.github.com>
2018-04-26 02:46:33 -0700

    Fixed spacing and exports

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,14 +1,17 @@
-const HTMLElement = require('./HTMLElement').default;
+const HTMLElement = require('./HTMLElement.js');
 // import HTMLElement from './HTMLElement'; ES6 unsupported by node -v 8
 
 // Define class here
+
 class DivElement extends HTMLElement {
   constructor(content) {
     super('div', content);
   }
 }
 
-module.exports.default = DivElement;
-// export default DivElement; // ES6 unsupported by node -v 8
+
 const andIThinkToMyself = new DivElement('What a wonderful world');
 console.log(andIThinkToMyself.render());
+
+module.exports = DivElement;
+// export default DivElement; // ES6 unsupported by node -v 8
