abd7448864a3ed6fa67c36efcaea8053dc012667
aallaouchiche <aallaouchiche@gmail.com>
2018-04-26 21:07:37 -0700

    Add files via upload
    No idea why some test are failing, like: HTMLElement class should have a function called render() where I do have a function render

---
+++ HTMLElement.js
@@ -0,0 +1,16 @@
+// Define class here
+class HTMLElement {
+  constructor(tag, element) {
+    this.tag = tag;
+    this.element = element;
+  }
+  render() {
+    return `<${this.tag}>${this.element}</${this.tag}>`;
+  }
+}
+
+const p = new HTMLElement('p', 'doing something here');
+p.render();
+
+// Export class here
+module.exports = { HTMLElement };
---
+++ Timer.js
@@ -0,0 +1,15 @@
+class Timer {
+  constructor(seconds) {
+    this.seconds = seconds;
+  }
+  start() {
+    const timerInterval = setInterval(() => {
+      if (this.seconds === 0) {
+        clearInterval(timerInterval);
+      }
+      this.seconds -= 1;
+    }, 1000);
+  }
+}
+
+module.exports = Timer;
