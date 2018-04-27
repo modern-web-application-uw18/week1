09b752919d28d89c43adf367efedfcbbdc98d252
Aghw <sam.asmerom@outlook.com>
2018-04-27 15:58:13 -0700

    finally, I'm able to finish this assignment without any errors.

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,17 +1,31 @@
-function Timer(seconds) {
-  this.seconds = seconds;
-}
+// function Timer(seconds) {
+//   this.seconds = seconds;
+// }
+//
+// Timer.prototype.start = function() {
+//   var instance = this;
+//   var timerInterval = setInterval(function() {
+//     if (instance.seconds === 0) {
+//       clearInterval(timerInterval);
+//     }
+//
+//     console.log(instance.seconds);
+//     instance.seconds -= 1;
+//   }, 1000);
+// };
 
-Timer.prototype.start = function() {
-  var instance = this;
-  var timerInterval = setInterval(function() {
-    if (instance.seconds === 0) {
-      clearInterval(timerInterval);
-    }
-
-    console.log(instance.seconds);
-    instance.seconds -= 1;
-  }, 1000);
+module.exports = class Timer {
+  constructor(seconds) {
+    this.seconds = seconds;
+  }
+  start() {
+    const timerInterval = setInterval(() => {
+      if (this.seconds === 0) {
+        clearInterval(timerInterval);
+      }
+      console.log(this.seconds);
+      this.seconds -= 1;
+    }, 1000);
+  }
 };
-
-module.exports = Timer;
+// module.exports = Timer;
