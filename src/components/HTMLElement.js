67a75a159308654150a4ddfa6a30fefdcf7858da
Jeff Kanakares <Jeff_kanakares@comcast.net>
2018-05-07 18:10:03 -0700

    Update HTMLElement.js

--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,19 @@
 // Define class here
+class ELMLElement {
+  constructor(tag, content) {
+    this.tag = tag;
+    this.content = content;
+  }
+  render() {
+    return `<${this.tag}>${this.content}</${this.tag}>`;
+  }
+}
+
+/* let foo = new HTMLElement("div", "goodbye");
+console.log(foo);
+let htmlString = foo.render();
+console.log(htmlString); */
 
 // Export class here
 module.exports = {};
+module.exports = HTMLElement;
