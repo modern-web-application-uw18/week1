e0546db9a0ef7478f83cf415628a9f522f4b8d6d
Jeff Kanakares <Jeff_kanakares@comcast.net>
2018-05-07 18:29:23 -0700

    Update Timer.js

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,6 +1,20 @@
 function Timer(seconds) {
   this.seconds = seconds;
-}
+  class Timer {
+  constructor(seconds) {
+    this.seconds = seconds;
+  }
+  start() {
+    const timerInterval = setInterval(() => {
+      if (this.seconds === 0) {
+        clearInterval(timerInterval);
+      }
+      console.log(this.seconds);
+        this.seconds -= 1;
+    }, 1000);
+  }
+ }
+
 
 Timer.prototype.start = function() {
   var instance = this;
@@ -8,7 +22,8 @@ Timer.prototype.start = function() {
     if (instance.seconds === 0) {
       clearInterval(timerInterval);
     }
-
+module.exports = Timer;
+    
     console.log(instance.seconds);
     instance.seconds -= 1;
   }, 1000);
