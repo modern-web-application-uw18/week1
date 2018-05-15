e8b0ab1d8056452f6b400407d33637041cc7d087
Jeff Kanakares <Jeff_kanakares@comcast.net>
2018-05-14 19:22:32 -0700

    Update DivElement.js

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,17 +1,10 @@
-const HTMLElement = require('./HTMLElement.js');
-
+const HTMLElement = require('./HTMLElement');
 
 // Define class here
-
 class DivElement extends HTMLElement {
   constructor(content) {
     super('div', content);
   }
 }
-
-const andIThinkToMyself = new DivElement('What a wonderful world');
-
-console.log(andIThinkToMyself.render());
-
 // Export class here
 module.exports = DivElement;
